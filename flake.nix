0c7eb0cd34286185a2d93948d6979f1acf367203
David Rumscheid <david.rumscheid@googlemail.com>
2024-02-20 01:21:32 +0100

    Fix: Add .gitignore for hardware-configuration

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.damaruNixOS = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
