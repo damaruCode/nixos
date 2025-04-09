f2f01c83b1cf785c20f8bedc4f2e5f34f38c14d0
damaruCode <david.rumscheid@googlemail.com>
2025-04-09 14:46:59 +0200

    Update: flake

--- flake.nix
+++ flake.nix
@@ -5,14 +5,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
-    nixosConfigurations.ace = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/ace/configuration.nix
-        ./modules
-      ];
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations.ace = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+        };
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/ace/configuration.nix
+          ./modules
+        ];
+      };
     };
-  };
 }
