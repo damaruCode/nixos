9bff853e1a8022a8a6665d3b3da7763fd5fab16c
damaruCode <david.rumscheid@gmail.com>
2025-11-09 22:05:24 +0100

    add tuf (wsl) conf

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "damarus NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
   };
 
   outputs =
-    { nixpkgs, ... }@inputs:
+    { nixpkgs, nixos-wsl, ... }@inputs:
     {
       nixosConfigurations.ace = nixpkgs.lib.nixosSystem {
         specialArgs = {
@@ -15,6 +16,22 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/ace/configuration.nix
+          ./modules
+        ];
+      };
+	
+    nixosConfigurations.tuf = nixpkgs.lib.nixosSystem {
+        specialArgs = {
+          inherit inputs;
+        };
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "25.05";
+            wsl.enable = true;
+          }
+	  ./hosts/tuf/configuration.nix
           ./modules
         ];
       };
---
+++ hosts/tuf/configuration.nix
@@ -0,0 +1,53 @@
+{ pkgs, ... }:
+{	
+
+# custom module options
+  greetd.enable = true;
+  nix-helpers.enable = true;
+
+ nix.settings = {
+    experimental-features = "nix-command flakes";
+    auto-optimise-store = true;
+  };
+
+ time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    useXkbConfig = true;
+  };
+
+services.xserver.enable = false;
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.variant = "altgr-intl";
+
+
+  nixpkgs.config.allowUnfree = true;
+
+ environment.systemPackages = with pkgs; [
+    gh
+    git
+    vim
+    zip
+    wget
+    cups
+    tree
+    unzip
+    git-lfs
+    neofetch
+    home-manager
+  ];
+
+programs.zsh.enable = true;
+
+users.users.damaru = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+
+  system.stateVersion = "25.05";
+}
